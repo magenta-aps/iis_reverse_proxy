40064411f7fa84b6823515d4e0a767c1c98250b9
Beemen Beshara <beemen@magenta-aps.dk>
2014-11-14 14:29:44 +0100

    Fie reorganisation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,52 +1,49 @@
 require.config({
-	paths: { 
+    paths: {
         'jquery': '//code.jquery.com/jquery-1.9.1.min',
         'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min'
     },
     shim: {
-        'bootstrap': [ 'jquery' ]
+        'bootstrap': ['jquery']
     }
 });
 
-define('callProcessQuery',['jquery','processQuery'],function($,p){
-	return function(){
-			var query = $('#query').val(); //get the content of the input field
-			var query2 = $('#query2').val(); //get the content of the input field
-			var online = ($("input[name=online]:checked").val() == "true");
-			p.processQuery(query, query2, online);
-	}
-})
+require(["jquery", "bootstrap", "processQuery", "validate", "modolus11"], function ($, b, p, v, m) {
 
-require(["jquery", "bootstrap", "processQuery", "validate", "modolus11","callProcessQuery"],function($,b,p,v,m,callProcessQuery) {
+    // wait for the document to be ready
+    $(function () {
+        // give focus to the search field
+        $('#query').focus();
 
-	// wait for the document to be ready
-	$(function() {
-	   	// give focus to the search field
-		$('#query').focus();
+        var callProcessQuery = function () {
+            var query = $('#query').val(); //get the content of the input field
+            var query2 = $('#query2').val(); //get the content of the input field
+            var online = ($("input[name=online]:checked").val() == "true");
+            p.processQuery(query, query2, online);
+        };
 
+        // handle submition routing
+        $('#quicksearchbutton').click(function (event) {
+            event.preventDefault();
+            callProcessQuery($);
+        });
 
-		// handle submition routing
-	   $('#quicksearchbutton').click(function(event) {
-		   event.preventDefault();
-		   callProcessQuery($);
-	   });
-	   
-	   // handle submition routing on enter as well
-	   $('#query').keypress(function(event) {
-		    if(event.which == 13) {
-		    	event.preventDefault();
-				callProcessQuery($);
-			}
-	   });
-	   
-	   // validate incomming input
-	   $('#query').keyup(function(event) {
-     	  var queryfield = $('#query'); // the query input field
-    	  var query = queryfield.val(); // value of the input field
-    	  var querygroup = $('#querygroup'); // form-group wrapping input
+        // handle submition routing on enter as well
+        $('#query').keypress(function (event) {
+            if (event.which == 13) {
+                event.preventDefault();
+                callProcessQuery($);
+            }
+        });
 
-	      v.validateQuery(queryfield, query, querygroup);
-	   });
-	}); //end ready
+        // validate incomming input
+        $('#query').keyup(function (event) {
+            var queryfield = $('#query'); // the query input field
+            var query = queryfield.val(); // value of the input field
+            var querygroup = $('#querygroup'); // form-group wrapping input
+
+            v.validateQuery(queryfield, query, querygroup);
+        });
+    }); //end ready
 });
 
