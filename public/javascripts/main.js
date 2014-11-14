cd877713d06a442a51642c9e30a60f55efe890bb
Beemen Beshara <beemen@magenta-aps.dk>
2014-11-14 14:20:48 +0100

    Isolated method callProcessQuery()

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,29 +8,34 @@ require.config({
     }
 });
 
-require(["jquery", "bootstrap", "processQuery", "validate", "modolus11"],function($,b,p,v,m) {
+define('callProcessQuery',['jquery','processQuery'],function($,p){
+	return function(){
+			var query = $('#query').val(); //get the content of the input field
+			var query2 = $('#query2').val(); //get the content of the input field
+			var online = ($("input[name=online]:checked").val() == "true");
+			p.processQuery(query, query2, online);
+	}
+})
+
+require(["jquery", "bootstrap", "processQuery", "validate", "modolus11","callProcessQuery"],function($,b,p,v,m,callProcessQuery) {
+
 	// wait for the document to be ready
 	$(function() {
 	   	// give focus to the search field
 		$('#query').focus();
-	   	
-	   // handle submition routing
+
+
+		// handle submition routing
 	   $('#quicksearchbutton').click(function(event) {
 		   event.preventDefault();
-		   var query = $('#query').val(); //get the content of the input field
-		   var query2 = $('#query2').val(); //get the content of the input field
-		   var online = ($("input[name=online]:checked").val() == "true");
-		   p.processQuery(query, query2,online);
+		   callProcessQuery($);
 	   });
 	   
 	   // handle submition routing on enter as well
 	   $('#query').keypress(function(event) {
 		    if(event.which == 13) {
 		    	event.preventDefault();
-		    	var query = $('#query').val(); //get the content of the input field
-				var query2 = $('#query2').val(); //get the content of the input field
-				var online = ($("input[name=online]:checked").val() == "true");
-				p.processQuery(query, query2,online);
+				callProcessQuery($);
 			}
 	   });
 	   
